286c14e0f443077831fc7cbdea4a2569ad10585d
Lxrdk <deathhell903@gmail.com>
2024-12-30 20:31:15 +0300

    ui bug fix

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -16,14 +16,14 @@ export function ProductCard({
   return (
     <Link
       href={`/products/${prodId}`}
-      className="group max-w-[175] max-h-[175] min-w-[175] min-h-[175] bg-primary-foreground rounded-md overflow-hidden flex relative border-2 "
+      className="group w-[175px] h-[175px] bg-primary-foreground rounded-md overflow-hidden flex relative border-2 "
     >
       <img
-        className="object-cover transition-all duration-300 ease-in-out group-hover:scale-105 "
+        className="object-cover w-full h-full transition-all duration-300 ease-in-out group-hover:scale-105"
         src={imageUrl}
-        alt=" product image"
+        alt="product image"
       />
-      <div className="rounded-md absolute bottom-0 h-full w-full z-50  bg-gradient-to-b from-transparent to-secondary-foreground text-white opacity-0 group-hover:opacity-100  transition-all duration-500 ease-in-out cursor-pointer">
+      <div className="rounded-md absolute bottom-0 h-full w-full z-50 bg-gradient-to-b from-transparent to-secondary-foreground text-white opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out cursor-pointer">
         <p className="bottom-0 absolute pl-1">{productName}</p>
       </div>
     </Link>
